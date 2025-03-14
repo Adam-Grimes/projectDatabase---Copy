91fe4529ceb0fba5ab0dcfce2c51ae5ee579f263
Adam Grimes <05adamg@gmail.com>
2025-03-14 10:55:21 +0000

    e

--- Public/assets/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('input', function(e) {
-    if (e.target.tagName.toLowerCase() === 'input' && e.target.type === 'text') {
-      const value = e.target.value;
-      if (value && value[0] !== value[0].toUpperCase()) {
-        e.target.value = value.charAt(0).toUpperCase() + value.slice(1);
-      }
-    }
-  });
-  
